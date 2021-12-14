09e53e9997fbd45fe54fab830236f629fe30e2bb
Oleg <oleg-novikov26@yandex.ru>
2021-12-14 16:51:22 +0300

    revert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { EditFormComponent } from './edit-form/edit-form.component';
 import { ListComponent } from './list/list.component';
 
 
 const routes: Routes = [
-
-  { path: '', component: ListComponent },
+  { path: '', component: AuthComponent },
+  { path: 'list', component: ListComponent },
   { path: 'book/:id', component: EditFormComponent },
 ];
 
