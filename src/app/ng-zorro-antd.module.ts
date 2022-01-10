32b8bacc6ecae26a8736f1afebad43e2eda6e44d
Oleg <oleg-novikov26@yandex.ru>
2022-01-10 15:40:50 +0300

    -

--- src/app/ng-zorro-antd.module.ts
+++ src/app/ng-zorro-antd.module.ts
@@ -1,6 +1,5 @@
 
 import { NzButtonModule } from 'ng-zorro-antd/button';
-
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzInputModule } from 'ng-zorro-antd/input';
